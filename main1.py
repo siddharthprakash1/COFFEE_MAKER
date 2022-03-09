8e0331dca4a2c8e07e40667e65ea81112ffdfb72
siddharthprakash1 <92435819+siddharthprakash1@users.noreply.github.com>
2022-03-09 13:02:48 +0530

    Add files via upload
    use main to see the exact code

---
+++ main1.py
@@ -0,0 +1,25 @@
+'''import turtle
+import another_module
+from turtle import Turtle,Screen
+print(another_module.another_variable)
+#timmy=turtle.Turtle()---(if only import turtle was written)
+#timmy is a object and Turtle is a class
+timmy=Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("blue")
+timmy.forward(100)
+my_screen=Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()'''
+'''from prettytable import PrettyTable
+#prettytable is a class and PrettyTable is an object and x is a attribute
+x=PrettyTable()
+#x.field_names=["POKEMON","TYPE"]
+#x.add_rows(["PIKACHU","ELECTRIC"])
+x.add_column("POKEMON ",["PIKACHU","CHARIZARD"])
+x.add_column("TYPE",["ELECTRIC","fire"])
+x.align='l'
+print(x)'''
+#CLASS IS A BLUEPRINT FOR CREATING A OBJECT AND A OBJECT HAS ATTRIBUTES
+
